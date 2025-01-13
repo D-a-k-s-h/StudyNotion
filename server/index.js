9b39ae7b7303d35053a089c9b02ff0baad25909e
D-a-k-s-h <daksh.kumar.ug22@gmail.com>
2025-01-13 14:07:45 +0530

    Updation in CORS policy

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"https://studynotion-backend-6vwl.onrender.com",
         credentials:true
     })
 )
