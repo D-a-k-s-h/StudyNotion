a6dddf4b0a0b8f63c6e59629e789956643b77c22
D-a-k-s-h <daksh.kumar.ug22@gmail.com>
2025-02-04 19:41:43 +0530

    Enhance layout and responsiveness of AccountSelector component

--- src/components/Common/AccountSelector.jsx
+++ src/components/Common/AccountSelector.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AccountSelector = ({tabData,field,setField}) => {
 
   return (
-    <div className='w-[195px] rounded-full bg-richblack-700 py-1 px-1 my-10 flex gap-1'>
+    <div className='w-fit rounded-full bg-richblack-700 py-1 px-1 my-10 flex gap-1'>
         {
           tabData.map((tab) => (
             <button key={tab.id} onClick={() => setField(tab.type)} className={`${field === tab.type ? 'bg-richblack-900 text-richblack-5 px-3 py-1 rounded-full transition-all duration-200' : 'px-3 py-1 bg-transparent text-richblack-100 hover:text-richblack-25'}`}>{tab?.tabName}</button>
